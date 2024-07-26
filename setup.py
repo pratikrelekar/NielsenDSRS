0b81a332b9a43a5e7526b0d72b6bb5862bd676b3
PratikR <52060680+pratikrelekar@users.noreply.github.com>
2024-07-25 23:10:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="Nielsen Reader’s main purpose is to facilitate ease of processing of Nielsen Retail Scanner data of Kilt’s Center’s Nielsen IQ data used for Academic research only. The striking feature of this library is Dask which acts as an underlying framework that uniquely empowers the user to read Nielsen data with limited on device resources (by processing larger-than-memory data in chunks and parallel fashion). It understands the Kilts/Nielsen directory structure.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/pratikrelekar/Nielsen-DSRS",
+    url="https://github.com/pratikrelekar/NielsenDSRS",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
