6cb37ff87b77dc1c7e9ffd97414e44a837aa693e
PratikR <52060680+pratikrelekar@users.noreply.github.com>
2024-08-12 17:30:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="NielsenRetail",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "dask[dataframe]==2024.1.1",
