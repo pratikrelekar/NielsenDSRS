2ba53023e4d8fac3aefc482aa993975d27117e01
PratikR <52060680+pratikrelekar@users.noreply.github.com>
2024-08-21 10:19:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="NielsenRetail",
-    version="0.1.2",
+    version="0.1.2",  # Update this with your new version
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
-        "dask[dataframe]==2024.1.1",
-        "distributed==2024.1.1",
-        "numpy==1.26.3",
-        "pandas==2.2.0",
-        "toolz==0.12.0"
+        "dask[dataframe]==2024.8.0",
+        "distributed==2024.8.0",
+        "numpy==2.0.1",
+        "pandas==2.2.2",
+        "toolz==0.12.1"
     ],
     author="Pratik Relekar, Hrishikesh Relekar, Matias Carrasco Kind",
     author_email="relekar2@illinois.edu, hrishkesh.relekar@chicagobooth.edu, mcarras2@illinois.edu",
-    description="Nielsen Reader’s main purpose is to facilitate ease of processing of Nielsen Retail Scanner data of Kilt’s Center’s Nielsen IQ data used for Academic research only. The striking feature of this library is Dask which acts as an underlying framework that uniquely empowers the user to read Nielsen data with limited on device resources (by processing larger-than-memory data in chunks and parallel fashion). It understands the Kilts/Nielsen directory structure.",
+    description="Nielsen Reader’s main purpose is to facilitate ease of processing of Nielsen Retail Scanner data...",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/pratikrelekar/NielsenDSRS",
